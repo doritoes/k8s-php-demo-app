c7b1b0e5f904e7fec306305c5114485ee0000253
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 10:22:33 -0500

    Update successful.php - linting

--- app/successful.php
+++ app/successful.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Webapp</title>
-   <link rel="stylesheet" href="css/style.css">
-</head>
-
-<main>
-<body>
-  <span class=header>SUCCESS</span>
-  <p>Successfully Registered</p>
-  <p><button onclick="location.href='index.php'">CLICK HERE TO LOGIN</button>
-</body>
-</main>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Webapp</title>
+     <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body class="main">
+    <span class=header>SUCCESS</span>
+    <p>Successfully Registered</p>
+    <p><button onclick="location.href='index.php'">CLICK HERE TO LOGIN</button>
+  </body>
 </html>
