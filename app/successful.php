e83fb441805925e537dab7fb82d33b3419ffdca6
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 09:23:24 -0500

    Create successful.php

---
+++ app/successful.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Webapp</title>
+  <style type="text/css">
+    body {
+      margin:0;
+      padding:0;
+      font-family: Sans-Serif;
+      line-height: 1.5em;
+    }	
+    header {
+      background: #26ade4;
+      height: 100px;
+      text-align:left;
+      font-size: 20px;
+      color:#FFFFFF
+    }
+    header we {
+      margin: 10px;
+      padding-top: 15px;
+      font-size:14px;
+    }
+    header strong {
+      margin: 0;
+      padding-top: 15px;
+      font-size:15px;
+    }
+    header a {
+      margin: 10px;
+      color:#FFFFFF;
+      font-size:14px;
+    }
+    main {
+      padding-bottom: 10010px;
+      margin-bottom: -10000px;
+      float: left;
+      width: 100%;
+    }
+    main button {
+      font-family: sans-serif;
+      text-transform: uppercase;
+      background: #26ade4;
+      border: 0;
+      padding: 15px;
+      color: #FFFFFF;
+      font-size: 12px;
+      text-align:center;
+    }
+  </style>
+</head>
+<header>
+SUCCESS
+</header>
+<main>
+<body>		
+  <p>Successfully Registered</p>
+  <p><button onclick="location.href='index.php'">CLICK HERE TO LOGIN</button>
+</body>
+</main>
+</html>
