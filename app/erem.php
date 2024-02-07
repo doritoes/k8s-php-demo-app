019961ab7b772d9a14d29a99fd8d49522f7c16f0
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 16:29:39 -0500

    Update erem.php - linting

--- app/erem.php
+++ app/erem.php
@@ -13,19 +13,19 @@ $login = htmlspecialchars($_SESSION['login']);
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>		
-    <div class="body">
+    <header class="body">
       <span class="welcome">Welcome &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
       <p><span class="Welcome"> Last logged in at : <?php echo $login; ?></span></p>
       <p><a href="logout.php">Log Out</a></p>
     </div>
-    <div class="main">
+    <main class="main">
       <span class="error">Please give correct password</span>
       <form action="rem.php" method="post">
         <p>Password : <input type="password" name="password"/></p>
         <p><input type="submit" value="CONFIRM"/></p>
       </form>		
-    </div>
-    <?php if($_SESSION['status']!="Active"){
+    </main>
+    <?php if($_SESSION['status']!="Active") {
       session_destroy();
       header("Location: index.php");
     } ?>
