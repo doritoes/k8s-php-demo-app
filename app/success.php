b4f81fca2034f071a9bf43567de76e4c49da720e
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 17:20:56 -0500

    Update success.php - linting

--- app/success.php
+++ app/success.php
@@ -14,7 +14,7 @@ $contact= htmlspecialchars($_SESSION['contact']);
 ?>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title><?php echo $configs[appname]; ?></title>
+    <title><?php echo $configs['appname']; ?></title>
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
