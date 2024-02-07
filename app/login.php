95af9983e0020ac838b8f401f4f440bc9116b5d2
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 10:24:47 -0500

    Update login.php - linting

--- app/login.php
+++ app/login.php
@@ -3,22 +3,22 @@ $configs = include('config.php');
 session_start();
 $_SESSION['status'] = "Active";
 $connection = mysqli_connect($configs[host], $configs[username], $configs[password], $configs[dbname]);
-if($connection === false){
+if ($connection === false) {
   die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 $name = mysqli_real_escape_string($connection, $_POST['uname']);
 $password = mysqli_real_escape_string($connection, $_POST['password']);
-$user =mysqli_query($connection,"SELECT email FROM app_user WHERE email='$name'");
-$pwd=mysqli_query($connection,"SELECT password FROM app_user WHERE email='$name'");
-$fname =mysqli_query($connection,"SELECT fname FROM app_user WHERE email='$name'");
-$lname =mysqli_query($connection,"SELECT lname FROM app_user WHERE email='$name'");
-$email =mysqli_query($connection,"SELECT email FROM app_user WHERE email='$name'");
-$dob =mysqli_query($connection,"SELECT dob FROM app_user WHERE email='$name'");
-$gender =mysqli_query($connection,"SELECT gender FROM app_user WHERE email='$name'");
-$contact =mysqli_query($connection,"SELECT contact FROM app_user WHERE email='$name'");
-$address =mysqli_query($connection,"SELECT address FROM app_user WHERE email='$name'");
-$login =mysqli_query($connection,"SELECT login FROM app_user WHERE email='$name'");
-$update=mysqli_query($connection,"UPDATE app_user set login=now()");
+$user = mysqli_query($connection,"SELECT email FROM app_user WHERE email='$name'");
+$pwd = mysqli_query($connection,"SELECT password FROM app_user WHERE email='$name'");
+$fname = mysqli_query($connection,"SELECT fname FROM app_user WHERE email='$name'");
+$lname = mysqli_query($connection,"SELECT lname FROM app_user WHERE email='$name'");
+$email = mysqli_query($connection,"SELECT email FROM app_user WHERE email='$name'");
+$dob = mysqli_query($connection,"SELECT dob FROM app_user WHERE email='$name'");
+$gender = mysqli_query($connection,"SELECT gender FROM app_user WHERE email='$name'");
+$contact = mysqli_query($connection,"SELECT contact FROM app_user WHERE email='$name'");
+$address = mysqli_query($connection,"SELECT address FROM app_user WHERE email='$name'");
+$login = mysqli_query($connection,"SELECT login FROM app_user WHERE email='$name'");
+$update = mysqli_query($connection,"UPDATE app_user set login=now()");
 $row1 = mysqli_fetch_row($user);
 $row2 = mysqli_fetch_row($pwd);
 $f = mysqli_fetch_row($fname);
