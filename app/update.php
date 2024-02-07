0c7d12b02e93f19546ff07ba71c9a974ee90c7fe
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 17:21:33 -0500

    Update update.php - linting

--- app/update.php
+++ app/update.php
@@ -9,7 +9,7 @@ $login = htmlspecialchars($_SESSION['login']);
 ?>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title><?php echo $configs[appname]; ?></title>
+    <title><?php echo $configs['appname']; ?></title>
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
