b5a4fa346a537f81bfafd616ab9905b40e6e2009
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 16:22:05 -0500

    Update remove.php - linting

--- app/remove.php
+++ app/remove.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+$configs = include('config.php');
 session_start();
-$fname = $_SESSION['fname'];
-$lname = $_SESSION['lname'];
-$login = $_SESSION['login'];
+$fname = htmlspecialchars($_SESSION['fname']);
+$lname = htmlspecialchars($_SESSION['lname']);
+$login = htmlspecialchars($_SESSION['login']);
 ?>
   <head>
-    <meta charset="UTF-8">
-    <title>Webapp</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title><?php echo $configs[appname]; ?></title>
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>		
-    <div class="body">
+    <header class="body">
       <span class="welcome">Welcome &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
       <p><span class="welcome">Last logged in at : <?php echo $login; ?></span></p>
       <p><a href="logout.php">Log Out</a></p>
-    </div>
-    <div class="main">
+    </header>
+    <main class="main">
       <form action="rem.php" method="post">
         <p>Password : <input type="password" name="password" required />
         <p><input type="submit" value="CONFIRM" />
       </form>		
-    </div>
-    <?php if($_SESSION['status']!="Active"){session_destroy();header("Location: index.php"); } ?>
+    </main>
+    <?php
+      if($_SESSION['status']!="Active") {
+        session_destroy();
+        header("Location: index.php");
+    } ?>
   </body>
 </html>
