a5fdd735e08ad4c807a8e870c49780772303e372
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 12:59:09 -0500

    Update rem.php - linting

--- app/rem.php
+++ app/rem.php
@@ -2,22 +2,28 @@
 $configs = include('config.php');
 session_start();
 $data = parse_ini_file("../config.ini");
-$connection= mysqli_connect($configs[host], $configs[username], $configs[password], $configs[dbname]);
-if($connection === false){
+$connection = mysqli_connect($configs[host], $configs[username], $configs[password], $configs[dbname]);
+if ($connection === false) {
   die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$a = $_SESSION['email'];
-$pwd = mysqli_real_escape_string($connection, $_POST['password']);
-$sql = "SELECT password from app_user where email='$a'";
-$query = mysqli_query($connection,$sql);
-$p = mysqli_fetch_row($query);
-if ($pwd==$p[0]) {
-mysqli_query($connection,"DELETE from app_user where password='$p[0]'");
-session_destroy();
-header("Location: index.php");
+$a = mysqli_real_escape_string($connection, $_SESSION['email'] ?? '');
+$pwd = mysqli_real_escape_string($connection, $_POST['password'] ?? '');
+
+$stmt = mysqli_prepare($connection, "SELECT password FROM app_user WHERE email = ?");
+mysqli_stmt_bind_param($stmt, "s", $email);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt, $credential);
+
+if (mysql_stmt_fetch($stmt) && password_verify($pwd, $credential)) {
+  // delete user
+  $stmt = mysqli_prepare($connection, "DELETE FROM app_user WHERE email = ?");
+  mysqli_stmt_bind_param($stmt, "s", $email);
+  if (mysqli_stmt_execute($stmt)) {
+    session_destroy();
+    header("Location: index.php");
+  }
 }
-else {
-  header("Location: erem.php");
-}
+Header("Location: erem.php");
+mysql_stmt_close($stmt);
 mysqli_close($connection); 
 ?>
