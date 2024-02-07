e042633a19c2afdbb12f89734a64cd7c88764718
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 16:17:15 -0500

    Update ecpd.php - linting

--- app/ecpd.php
+++ app/ecpd.php
@@ -8,7 +8,7 @@ $lname = htmlspecialchars($_SESSION['lname']);
 $login = htmlspecialchars($_SESSION['login']);
 ?>
   <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title><?php echo $configs[appname]; ?></title>
     <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
