c824443df5974f2b7c0b328775f46a796ed6912d
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 11:50:46 -0500

    Update ecpd.php - linting

--- app/ecpd.php
+++ app/ecpd.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+$configs = include('config.php');
 session_start();
-$fname = $_SESSION['fname'];
-$lname = $_SESSION['lname'];
-$login = $_SESSION['login'];
+$fname = htmlspecialchars($_SESSION['fname']);
+$lname = htmlspecialchars($_SESSION['lname']);
+$login = htmlspecialchars($_SESSION['login']);
 ?>
   <head>
     <meta charset="UTF-8">
-    <title>Webapp</title>
+    <title><?php echo $configs[appname]; ?></title>
     <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
     <script type="text/javascript">
@@ -16,25 +17,23 @@ $(document).ready(function() {
   $("#password2").keyup(validate);
 });
 function validate() {
-  var password1 = $("#password1").val();
-  var password2 = $("#password2").val();
-  if(password1 == password2 && password1!=null && password2!=null) {
-    $("#message").html("<font color=\"green\"><b>Passwords Match</b></font>");
-    $("input[type=submit]").prop("disabled", false);      
-  } else {
-    $("#message").html("<font color=\"red\"><b>Check Passwords</b></font>");
-    $("input[type=submit]").prop("disabled", true); 
-  }
+  var newPassword = $("#password1").val();
+  var confirmedPassword = $("#password2").val();
+  var passwordMismatch = newPassword !== confirmed Password;
+  $("#message").html(passwordMismatch
+    ? '<font color="red"><b>Passwords don\'t match</b></font>'
+    : '<font color="green"><b>Passwords match</b></font>');
+  $("input[type=submit]").prop("disabled", passwordMismatch);
 }
     </script>
   </head>
   <body>		
-    <div class="body">
+    <header class="body">
       <span class="welcome"><strong>Welcome</strong> &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
       <p><span class="welcome"> Last logged in at : <?php echo $login; ?></span></p>
       <p> <a href="logout.php">Log Out</a>
-    </div>
-    <div class="main">
+    </header>
+    <main class="main">
       <span class="error">Please give correct password</span>
       <form action="cup.php" method="post">
         <p>Current Password : <input type="password" name="password" placeholder="current password" required/>
@@ -43,7 +42,10 @@ function validate() {
         <span id="message"></span>
         <p><input type="submit" value="CHANGE PASSWORD" id="submit"disabled="disabled">
       </form>		
-    </div>
-    <?php if ($_SESSION['status']!="Active"){session_destroy();header("Location: index.php"); } ?>
+    </main>
+    <?php if ($_SESSION['status']!="Active") {
+      session_destroy();
+      header("Location: index.php");
+    } ?>
   </body>
 </html>
