a73d6c3e91d6cb7eee81ccd95fc92793e7a2d98d
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-07 16:23:42 -0500

    Update signup.php - linting

--- app/signup.php
+++ app/signup.php
@@ -1,8 +1,11 @@
 <!DOCTYPE HTML>
 <html lang="en">
+<?php
+$configs = include('config.php');
+?>
   <head>
     <meta charset="UTF-8">
-    <title>Webapp</title>
+    <title><?php echo $configs[appname]; ?></title>
     <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
     <script type="text/javascript">
