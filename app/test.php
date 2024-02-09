060f28b7677ef65859989ee78588accc31cbb35c
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-09 18:02:11 -0500

    Update test.php

--- app/test.php
+++ app/test.php
@@ -2,11 +2,23 @@
 $configs = include('conf/config.php');
 session_start();
 try {
-    $connection = mysqli_connect($configs['host'], $configs['username'], $configs['password'], $configs['dbname']);
-    if (!$connection) {
-        throw new Exception('Database connection failed: ' . mysqli_connect_error());
-    }
-    // Proceed with database operations
+  $connection = mysqli_connect($configs['host'], $configs['username'], $configs['password'], $configs['dbname']);
+  if (!$connection) {
+    throw new Exception('Database connection failed: ' . mysqli_connect_error());
+  }
+  // Proceed with database operations
+  $create = "CREATE TABLE IF NOT EXISTS app_user (
+             fname varchar(255),
+             lname varchar(255),
+             email varchar(255) Primary Key,
+             password varchar(50),
+             address varchar(255),
+             gender varchar(5),
+             contact varchar(15),
+             dob date,
+             login timestamp)";
+  mysqli_query($connection, $create);
+}
 } catch (Exception $e) {
     // Handle errors gracefully
     header('HTTP/1.1 503 Service Unavailable'); // Set appropriate HTTP status code
