8d22d43f3ff561a0f96f25378b1923b392272845
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-10 18:10:06 -0500

    Update ecpd.php

--- app/ecpd.php
+++ app/ecpd.php
@@ -19,38 +19,57 @@ $login = htmlspecialchars($_SESSION['login']);
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js"></script>
     <script type="text/javascript">
 $(document).ready(function() {
-  $("#password2").keyup(validate);
+$("#password2").keyup(validate);
 });
 function validate() {
-  var newPassword = $("#password1").val();
-  var confirmedPassword = $("#password2").val();
-  var passwordMismatch = newPassword !== confirmedPassword;
-  $("#message").html(passwordMismatch
-    ? '<font color="red"><b>Passwords don\'t match</b></font>'
-    : '<font color="green"><b>Passwords match</b></font>');
-  $("input[type=submit]").prop("disabled", passwordMismatch);
-}
+  var password1 = $("#password1").val();
+  var password2 = $("#password2").val();
+  if(password1 == password2 && password1!=null && password2!=null) {
+    $("#message").html("<font color=\"green\"><b>Passwords Match</b></font>");
+    $("input[type=submit]").prop("disabled", false);      
+  } else {
+    $("#message").html("<font color=\"red\"><b>Check Passwords</b></font>");
+    $("input[type=submit]").prop("disabled", true); 
+    }
+  }
     </script>
   </head>
   <body>		
-    <header class="body">
-      <span class="welcome"><strong>Welcome</strong> &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
-      <p><span class="welcome"> Last logged in at : <?php echo $login; ?></span></p>
-      <p> <a href="logout.php">Log Out</a>
+    <header>
+      <div class="body">
+        <span class="welcome"><strong>Welcome</strong> &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
+        <span class="welcome"> Last logged in at: <?php echo $login; ?></span>
+        <nav>
+          <a href="success.php">Home</a>
+        </nav>
+        <nav>
+          <a href="logout.php">Log Out</a>
+        </nav>
+      </div>
     </header>
-    <main class="main">
+    <main>
       <span class="error">Please provide the correct password</span>
       <form action="cup.php" method="post">
-        <p>Current Password : <input type="password" name="password" placeholder="current password" required/>
-        <p>Enter New Password :<input type="password" name="password1" id ="password1" placeholder="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
-        <p>Re-enter New Password :<input type="password" name="password2" id ="password2"placeholder="re enter password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title ="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
-        <span id="message"></span>
-        <p><input type="submit" value="CHANGE PASSWORD" id="submit"disabled="disabled">
+        <fieldset>
+          <legend>Change Password</legend>
+          <p>
+            <label for="password">Current Password:</label>
+            <input type="password" name="password" id="password" placeholder="current password" required>
+          </p>
+          <p>
+            <label for="password1">New Password:</label>
+            <input type="password" name="password1" id ="password1" placeholder="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+          </p>
+          <p>
+            <label for="password2">Re-enter New Password:</label>
+            <input type="password" name="password2" id ="password2" placeholder="re enter password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title ="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+          </p>
+          <span id="message"></span>
+          <p>
+            <input type="submit" value="CHANGE PASSWORD" id="submit" disabled="disabled" aria-label="Change Password Button">
+          </p>
+        </fieldset>
       </form>		
     </main>
-    <?php if ($_SESSION['status'] != "Active") {
-      session_destroy();
-      header("Location: index.php");
-    } ?>
   </body>
 </html>
