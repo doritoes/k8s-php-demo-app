9296985c2b418fe3ab2ab4b4b651b5c172c88684
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-10 17:08:44 -0500

    Update success.php - linting

--- app/success.php
+++ app/success.php
@@ -25,8 +25,8 @@ $contact= htmlspecialchars($_SESSION['contact']);
   <body>
     <header>
       <div class="body">
-        <span class="welcome">Welcome &nbsp<?php echo $fname; echo " " ; echo $lname;?></span>
-        <p><span class="welcome">Last logged in at :<?php echo $login; ?></span></p>
+        <span class="welcome"><strong>Welcome</strong> &nbsp<?php echo $fname; echo " " ; echo $lname;?></span>
+        <p><span class="welcome">Last logged in at: <?php echo $login; ?></span></p>
         <p><a href="logout.php">Log Out</a></p>
       </div>
     </header>
