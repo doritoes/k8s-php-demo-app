72edbf2ed436828e47aefbf110e41cbd104cbd7a
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-09 20:58:58 -0500

    Update successful.php - update

--- app/successful.php
+++ app/successful.php
@@ -6,7 +6,7 @@ $configs = include('conf/config.php');
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title><?php echo $configs['appname']; ?></title>
-     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
     <header>
