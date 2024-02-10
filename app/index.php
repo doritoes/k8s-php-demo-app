d68b7f438d0c6be4340d92f450b5a1318615c04f
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-09 20:54:24 -0500

    Update index.php - linting

--- app/index.php
+++ app/index.php
@@ -13,9 +13,9 @@ session_start();
     <div class="login-page">
       <div class="form">
         <form action="login.php" method="post">
-          <input type="text" placeholder="username" name="uname" required />
-          <input type="password" placeholder="password" name="password" required />
-          <input type="submit" value="login" />
+          <input type="text" placeholder="username" name="uname" required>
+          <input type="password" placeholder="password" name="password" required>
+          <input type="submit" value="login">
           <p class="message">Not registered? <a href="signup.php">Create an account</a></p>
         </form>
       </div>
