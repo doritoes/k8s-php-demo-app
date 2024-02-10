d7d6b4678a1e4fffcadfa99cb12d0ebc1b026da1
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-10 18:10:03 -0500

    Update cpd.php

--- app/cpd.php
+++ app/cpd.php
@@ -1,6 +1,11 @@
 <?php
 $configs = include('conf/config.php');
 session_start();
+if (!isset($_SESSION['status']) || $_SESSION['status'] !== "Active") {
+    // Session is not active or invalid
+    header("Location: loggedout.php"); // Redirect to an error page
+    exit(); // Stop further execution of the current page
+}
 $fname = htmlspecialchars($_SESSION['fname']);
 $lname = htmlspecialchars($_SESSION['lname']);
 $login = htmlspecialchars($_SESSION['login']);
@@ -33,7 +38,7 @@ function validate() {
     <header>
       <div class="body">
         <span class="welcome"><strong>Welcome</strong> &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
-        <p><span class="welcome">Last logged in at : <?php echo $login; ?></span></p>
+        <p><span class="welcome">Last logged in at: <?php echo $login; ?></span></p>
         <nav>
           <a href="success.php">Home</a>
         </nav>
