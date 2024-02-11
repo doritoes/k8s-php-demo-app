f4016fe8b141c8f4e9b3c3b214526df2bca92565
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-10 21:21:47 -0500

    Update remove.php

--- app/remove.php
+++ app/remove.php
@@ -5,7 +5,7 @@ $configs = include('conf/config.php');
 session_start();
 if (!isset($_SESSION['status']) || $_SESSION['status'] !== "Active") {
   // Session is not active or invalid
-  header("Location: loggedout.php"); // Redirect to an error page
+  header("Location: login-again.php"); // Redirect to an error page
   exit(); // Stop further execution of the current page
 }
 $fname = htmlspecialchars($_SESSION['fname']);
@@ -23,7 +23,7 @@ $login = htmlspecialchars($_SESSION['login']);
         <span class="welcome"><strong>Welcome</strong> &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
         <p><span class="welcome">Last logged in at: <?php echo $login; ?></span></p>
         <nav>
-          <a href="success.php">Home</a>
+          <a href="index.php">Home</a>
         </nav>
         <nav>
           <a href="logout.php">Log Out</a>
