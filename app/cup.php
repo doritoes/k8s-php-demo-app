1608bc19b455e7cad207fbf1d49cc7096391a2aa
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-10 19:20:38 -0500

    Update cup.php

--- app/cup.php
+++ app/cup.php
@@ -22,21 +22,24 @@ $pwd = mysqli_real_escape_string($connection, $_POST['password'] ?? '');
 $npwd = mysqli_real_escape_string($connection, $_POST['password1'] ?? '');
 
 // Insecure password handling - convert to using password hashes
-// Look up password - convert to using email and password in the select, and file on rows = 0
 $stmt = mysqli_prepare($connection, "SELECT password FROM app_user WHERE email = ?");
 mysqli_stmt_bind_param($stmt, "s", $a);
-mysqli_stmt_execute($stmt);
-mysqli_stmt_bind_result($stmt, $saved_credential);
-mysqli_stmt_fetch($stmt);
-if ($pwd == $saved_credentials) {
-  // Prepared statement for password update
-  $stmt = mysqli_prepare($connection, "UPDATE app_user SET password = ? WHERE email = ?");
-  mysqli_stmt_bind_param($stmt, "ss", $npwd, $a);
-  mysqli_stmt_execute($stmt);
-  header("Location: index.php");
-  session_destroy();
-} else {
-  header("Location: ecpd.php");
+if (mysqli_stmt_execute($stmt)) {
+  mysqli_stmt_bind_result($stmt, $credential);
+  if (mysqli_stmt_fetch($stmt)) {
+    if ($pwd === $credential) {
+      // Prepared statement for password update
+      $stmt = mysqli_prepare($connection, "UPDATE app_user SET password = ? WHERE email = ?");
+      mysqli_stmt_bind_param($stmt, "ss", $npwd, $a);
+      mysqli_stmt_execute($stmt);
+      header("Location: index.php");
+      session_destroy();
+      mysqli_stmt_close($stmt);
+      mysqli_close($connection);
+      exit;
+    }
+  }
 }
 mysqli_close($connection);
+header("Location: ecpd.php");
 ?>
