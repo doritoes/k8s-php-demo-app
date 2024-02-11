ad28ffe65341bc24619bfc45588c9388151412e3
doritoes <39832079+doritoes@users.noreply.github.com>
2024-02-11 11:33:44 -0500

    Update unregister.php

--- app/unregister.php
+++ app/unregister.php
@@ -19,16 +19,14 @@ $login = htmlspecialchars($_SESSION['login']);
   </head>
   <body>		
     <header>
-      <div class="body">
-        <span class="welcome"><strong>Welcome</strong> &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
-        <p><span class="welcome">Last logged in at: <?php echo $login; ?></span></p>
-        <nav>
-          <a href="index.php">Home</a>
-        </nav>
-        <nav>
-          <a href="logout.php">Log Out</a>
-        </nav>
-      </div>
+      <span class="welcome"><strong>Welcome</strong> &nbsp<?php echo $fname; echo " " ; echo $lname ;?></span>
+      <p><span class="welcome">Last logged in at: <?php echo $login; ?></span></p>
+      <nav>
+        <a href="index.php">Home</a>
+      </nav>
+      <nav>
+        <a href="logout.php">Log Out</a>
+      </nav>
     </header>
     <main>
       <div class="form">
